62ef11347fe9f30a8318766b1ef22300d2369072
root <root@puppet.ciclos.valledeljerte3>
2025-07-04 09:40:01 +0200

    Backup automático: 2025-07-04 09:40:01

--- modules/instalacionapps/manifests/vmware_linux.pp
+++ modules/instalacionapps/manifests/vmware_linux.pp
@@ -5,4 +5,25 @@
 # @example
 #   include instalacionapps::vmware_linux
 class instalacionapps::vmware_linux {
+
+  package { ['gcc', 'make', "linux-headers-${facts['kernelrelease']}"]:
+    ensure => installed,
+  }
+
+  $bundle_path = '/tmp/VMware.bundle'
+
+  file { $bundle_path:
+    source => 'puppet:///modules/instalacionapps/VMware-Workstation-FULL.bundle',
+    mode   => '0755',
+  }
+
+  exec { 'instalar_vmware':
+    command => "$bundle_path --eulas-agreed --required --console",
+    path    => ['/bin', '/usr/bin'],
+    creates => '/usr/bin/vmware',
+    require => [
+      Package['gcc'],
+      File[$bundle_path],
+    ],
+  }
 }

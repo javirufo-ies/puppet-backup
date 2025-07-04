4ee69656588bc19cd8a500bc442a6a67bf57289b
root <root@puppet.ciclos.valledeljerte3>
2025-07-04 09:15:01 +0200

    Backup automático: 2025-07-04 09:15:01

---
+++ modules/instalacionapps/manifests/vmware_linux.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include instalacionapps::vmware_linux
+class instalacionapps::vmware_linux {
+}

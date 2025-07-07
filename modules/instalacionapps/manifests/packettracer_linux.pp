f6bc368c269a2b0c8a721778aa7ba59874cdae10
root <root@puppet.ciclos.valledeljerte3>
2025-07-07 10:20:01 +0200

    Backup automático: 2025-07-07 10:20:01

--- modules/instalacionapps/manifests/packettracer_linux.pp
+++ modules/instalacionapps/manifests/packettracer_linux.pp
@@ -5,53 +5,50 @@
 # @example
 #   include instalacionapps::packettracer_linux
 class instalacionapps::packettracer_linux {
+
+
+  package { 'nfs-common':
+    ensure => installed,
+  }
+
+
+# 1. Crea directorio de montaje temporal
+file { '/tmp/nas_instaladores':
+  ensure => 'directory',
 }
-class instalacionapps::virtualbox_linux {
 
-  # Dependencias necesarias
-  package { ['wget', 'gnupg']: ensure => installed }
+# 2. Monta el recurso NFS temporalmente
+mount { '/tmp/nas_instaladores':
+  ensure  => 'mounted',
+  device  => '10.0.0.21:/Repositorio/Instaladores',
+  fstype  => 'nfs',
+  options => 'ro',
+  atboot  => false,
+  require => File['/tmp/nas_instaladores'],
+}
 
-  # Añadir la clave pública de Oracle para VirtualBox → usando keyring
-  exec { 'add_virtualbox_key':
-    command => 'wget -qO- https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor | tee /etc/apt/keyrings/oracle-virtualbox.gpg > /dev/null',
-    creates => '/etc/apt/keyrings/oracle-virtualbox.gpg',
-    path    => ['/usr/bin', '/bin'],
-  }
+# 3. Copia el instalador, solo si no existe
+exec { 'copiar_packet_desde_nfs':
+  command => '/usr/bin/cp /tmp/nas_instaladores/Packet_Tracer822_amd64_signed.deb /tmp/packet.deb',
+  creates => '/tmp/packet.deb',
+  require => Mount['/tmp/nas_instaladores'],
+}
 
-  # Configurar el repositorio oficial de VirtualBox (Ubuntu/Debian)
-  exec { 'add_virtualbox_repo':
-    command => 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/oracle-virtualbox.gpg] https://download.virtualbox.org/virtualbox/debian noble contrib" > /etc/apt/sources.list.d/virtualbox.list && apt-get update',
-    unless  => 'test -f /etc/apt/sources.list.d/virtualbox.list',
-    require => Exec['add_virtualbox_key'],
-    path    => ['/usr/bin', '/bin'],
-  }
-
-  # Instalación de VirtualBox
-  package { 'virtualbox-7.0':
-    ensure  => present,
-    require => Exec['add_virtualbox_repo'],
-  }
+exec { 'instalar_packet':
+  command => 'dpkg -i /tmp/packet.deb && rm /tmp/packet.deb',
+  path    => ['/bin', '/usr/bin'],
+  require => Exec['copiar_packet_desde_nfs'],
+}
 
 
+# 4. Desmonta NFS solo si se montó antes
+exec { 'desmontar_nas':
+  command     => '/usr/bin/umount /tmp/nas_instaladores',
+  onlyif      => '/usr/bin/mount | /usr/bin/grep /tmp/nas_instaladores',
+  require     => Exec['copiar_packet_desde_nfs'],
+  path        => ['/bin', '/usr/bin'],
+}
 
 
-
-  # Instalación del Extension Pack
-  $extpack_version = '7.0.18'
-  $extpack_file = "Oracle_VM_VirtualBox_Extension_Pack-${extpack_version}.vbox-extpack"
-  $extpack_url = "https://download.virtualbox.org/virtualbox/${extpack_version}/${extpack_file}"
-
-  exec { 'download_virtualbox_extpack':
-    command => "/usr/bin/wget -O /tmp/${extpack_file} ${extpack_url}",
-    creates => "/tmp/${extpack_file}",
-    require => Package['virtualbox-7.0'],
-  }
-
-  exec { 'install_virtualbox_extpack':
-    command => "yes | /usr/bin/VBoxManage extpack install --replace /tmp/${extpack_file}",
-    unless  => "/usr/bin/VBoxManage list extpacks | grep -q 'Version: ${extpack_version}'",
-    require => Exec['download_virtualbox_extpack'],
-    path    => ['/usr/bin', '/bin'],
-  }
 
 }

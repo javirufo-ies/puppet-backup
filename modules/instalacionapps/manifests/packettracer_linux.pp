6112787e2eaee5f1dec87efdd465ce55f86090db
root <root@puppet.ciclos.valledeljerte3>
2025-07-07 10:45:01 +0200

    Backup automático: 2025-07-07 10:45:01

--- modules/instalacionapps/manifests/packettracer_linux.pp
+++ modules/instalacionapps/manifests/packettracer_linux.pp
@@ -9,24 +9,8 @@ class instalacionapps::packettracer_linux {
 
 
 
-# 1. Crea directorio de montaje temporal
-file { '/tmp/nas_instaladores':
-  ensure => 'directory',
-}
-
-# 2. Monta el recurso NFS temporalmente
-mount { '/tmp/nas_instaladores':
-  ensure  => 'mounted',
-  device  => '10.0.0.21:/Repositorio/Instaladores',
-  fstype  => 'nfs',
-  options => 'ro',
-  atboot  => false,
-  require => File['/tmp/nas_instaladores'],
-}
-
-# 3. Copia el instalador, solo si no existe
-exec { 'copiar_packet_desde_nfs':
-  command => '/usr/bin/cp /tmp/nas_instaladores/Packet_Tracer822_amd64_signed.deb /tmp/packet.deb',
+exec { 'copiar_packet':
+  command => "smbclient //10.0.0.21/Repositorio -N -c 'cd Instaladores; get Packet_Tracer822_amd64_signed.deb /tmp/packettracer.deb'",
   creates => '/tmp/packet.deb',
   require => Mount['/tmp/nas_instaladores'],
 }
@@ -34,17 +18,9 @@ exec { 'copiar_packet_desde_nfs':
 exec { 'instalar_packet':
   command => 'dpkg -i /tmp/packet.deb && rm /tmp/packet.deb',
   path    => ['/bin', '/usr/bin'],
-  require => Exec['copiar_packet_desde_nfs'],
+  require => Exec['copiar_packet'],
 }
 
-
-# 4. Desmonta NFS solo si se montó antes
-exec { 'desmontar_nas':
-  command     => '/usr/bin/umount /tmp/nas_instaladores',
-  onlyif      => '/usr/bin/mount | /usr/bin/grep /tmp/nas_instaladores',
-  require     => Exec['copiar_packet_desde_nfs'],
-  path        => ['/bin', '/usr/bin'],
-}
 
 
 

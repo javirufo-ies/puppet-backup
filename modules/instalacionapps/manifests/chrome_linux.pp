54422de66e402b2325f5f6fff038a6e7adfd447a
root <root@puppet.ciclos.valledeljerte3>
2025-07-07 08:30:01 +0200

    Backup automático: 2025-07-07 08:30:01

--- modules/instalacionapps/manifests/chrome_linux.pp
+++ modules/instalacionapps/manifests/chrome_linux.pp
@@ -4,5 +4,30 @@
 #
 # @example
 #   include instalacionapps::chrome_linux
+
 class instalacionapps::chrome_linux {
+
+  # Asegura que el módulo puppetlabs-apt está cargado
+  include apt
+
+  # Añadir el repositorio oficial de Google Chrome
+  apt::source { 'google-chrome':
+    location => 'https://dl.google.com/linux/chrome/deb/',
+    repos    => 'stable main',
+    key      => {
+      'id'     => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+      'source' => 'https://dl.google.com/linux/linux_signing_key.pub',
+    },
+    include  => {
+      src => false,
+    },
+  }
+
+  # Instalar Google Chrome estable
+  package { 'google-chrome-stable':
+    ensure  => installed,
+    require => Apt::Source['google-chrome'],
+  }
+
+
 }

31c24102a4985b6fa0b244fdb7b813612cf1361a
root <root@puppet.ciclos.valledeljerte3>
2025-07-07 12:25:01 +0200

    Backup automático: 2025-07-07 12:25:01

--- modules/instalacionapps/manifests/vmware_linux.pp
+++ modules/instalacionapps/manifests/vmware_linux.pp
@@ -7,29 +7,12 @@
 class instalacionapps::vmware_linux {
 
 
-
-
-# 1. Crea directorio de montaje temporal
-file { '/tmp/nas_instaladores':
-  ensure => 'directory',
+exec { 'copiar_vmware':
+	command => "smbclient //10.0.0.21/Repositorio -N -c 'cd Instaladores; get VMware-Workstation-Full-17.6.3-24583834.x86_64.bundle /tmp/VMware.bundle',
+	creates => '/tmp/VMware.bundle',
+	path =>	['/usr/bin', '/bin'],
 }
 
-# 2. Monta el recurso NFS temporalmente
-mount { '/tmp/nas_instaladores':
-  ensure  => 'mounted',
-  device  => '10.0.0.21:/Repositorio/Instaladores',
-  fstype  => 'nfs',
-  options => 'ro',
-  atboot  => false,
-  require => File['/tmp/nas_instaladores'],
-}
-
-# 3. Copia el instalador, solo si no existe
-exec { 'copiar_vmware_desde_nfs':
-  command => '/usr/bin/cp /tmp/nas_instaladores/VMware-Workstation-Full-17.6.3-24583834.x86_64.bundle /tmp/VMware.bundle',
-  creates => '/tmp/VMware.bundle',
-  require => Mount['/tmp/nas_instaladores'],
-}
 
 exec { 'instalar_vmware':
   command => 'sh /tmp/VMware.bundle --eulas-agreed --console --required && rm /tmp/VMware.bundle',
@@ -39,13 +22,6 @@ exec { 'instalar_vmware':
 }
 
 
-# 4. Desmonta NFS solo si se montó antes
-exec { 'desmontar_nas':
-  command     => '/usr/bin/umount /tmp/nas_instaladores',
-  onlyif      => '/usr/bin/mount | /usr/bin/grep /tmp/nas_instaladores',
-  require     => Exec['copiar_vmware_desde_nfs'],
-  path        => ['/bin', '/usr/bin'],
-}
 
 
 

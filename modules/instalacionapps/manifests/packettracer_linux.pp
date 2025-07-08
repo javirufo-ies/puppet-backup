a4c5e68795ae8b79698a28ad73ee15ec2412ddca
Javi Rufo <javirufo@iesvjp.es>
2025-07-08 09:30:01 +0200

    Backup automático: 2025-07-08 09:30:01

--- modules/instalacionapps/manifests/packettracer_linux.pp
+++ modules/instalacionapps/manifests/packettracer_linux.pp
@@ -6,6 +6,10 @@
 #   include instalacionapps::packettracer_linux
 class instalacionapps::packettracer_linux {
 
+package {['sudo','dialog','xdg-utils','gtk-update-icon-cache','libgl1-mesa-glx','libpulse0','libnss3','libxss1','libasound2','libxslt1.1','libxkbcommon-x11-0','libxcb-xinerama0-dev','libfreetype6','libc6','libstdc++6'],
+	ensure => installed,
+	}
+
 exec { 'copiar_packet':
 	command => "smbclient //10.0.0.21/Repositorio -N -c 'cd Instaladores; get Packet_Tracer822_amd64_signed.deb /tmp/packet.deb'",
 	creates => '/tmp/packet.deb',

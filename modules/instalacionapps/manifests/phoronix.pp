72c32daaa8ce8a5b23b7f3c9265a33d243790834
Javi Rufo <javirufo@iesvjp.es>
2025-07-11 12:35:01 +0200

    Backup automático: 2025-07-11 12:35:01

--- modules/instalacionapps/manifests/phoronix.pp
+++ modules/instalacionapps/manifests/phoronix.pp
@@ -15,7 +15,6 @@ class instalacionapps::phoronix {
   # Descargar el .deb de Phoronix Test Suite
   exec { 'descargar_phoronix':
     command => 'wget -O /tmp/master.zip https://github.com/phoronix-test-suite/phoronix-test-suite/archive/refs/heads/master.zip && unzip /tmp/master.zip',
-    creates => '/tmp/install.sh',
     path    => ['/usr/bin', '/bin'],
   }
 

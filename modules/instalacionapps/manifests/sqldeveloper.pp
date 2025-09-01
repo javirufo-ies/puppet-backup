7053f60afcc44228677dcfa30950b2b72dab69eb
Javi Rufo <javirufo@iesvjp.es>
2025-09-01 10:10:01 +0200

    Backup automático: 2025-09-01 10:10:01

--- modules/instalacionapps/manifests/sqldeveloper.pp
+++ modules/instalacionapps/manifests/sqldeveloper.pp
@@ -7,7 +7,7 @@
 class instalacionapps::sqldeveloper {
 	$nombreScript = 'c:\\tmp\scriptSQLDeveloper.ps1'
 #	$directory_path = 'C:\\Program Files\\SQLDeveloper'
-	$directory_path = 'C:\\PRogram Files'
+	$directory_path = 'C:\\Program Files'
 	$source_file = '\\\10.0.0.21\Instaladores\sqldeveloper-23.1.1.345.2114-x64.zip'
 	$destination_file = 'C:\\tmp\\sqldeveloper-23.1.1.345.2114-x64.zip'
 	$powershell_path = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
@@ -15,12 +15,12 @@ class instalacionapps::sqldeveloper {
 
 #script que lleva a cabo la copia y ejecución de la instalación del paquete
 	$contenidoScript = "
-		if (!(Test-Path -Path \"${directory_path}\")) {
+		if (!(Test-Path -Path \"${directory_path}\SQLDeveloper")) {
 			Copy-Item -Path \"${source_file}\" -Destination \"${destination_file}\"
 			Expand-Archive \"$destination_file\" -DestinationPath \"${directory_path}\"
-#			Remove-Item -Path \"${destination_file}\"
+			Remove-Item -Path \"${destination_file}\"
 		}
-#		Remove-Item -Path \$MyInvocation.MyCommand.Path"
+		Remove-Item -Path \$MyInvocation.MyCommand.Path"
 
 
 # Crear el archivo de script con el contenido definido

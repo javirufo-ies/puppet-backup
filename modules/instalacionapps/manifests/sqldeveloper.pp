0c132410311cff493234a725050882746ad3686f
Javi Rufo <javirufo@iesvjp.es>
2025-09-01 10:15:01 +0200

    Backup automático: 2025-09-01 10:15:01

--- modules/instalacionapps/manifests/sqldeveloper.pp
+++ modules/instalacionapps/manifests/sqldeveloper.pp
@@ -6,8 +6,8 @@
 #   include instalacionapps::sqldeveloper
 class instalacionapps::sqldeveloper {
 	$nombreScript = 'c:\\tmp\scriptSQLDeveloper.ps1'
-#	$directory_path = 'C:\\Program Files\\SQLDeveloper'
-	$directory_path = 'C:\\Program Files'
+	$directory_path = 'C:\\Program Files\\SQLDeveloper'
+#	$directory_path = 'C:\\Program Files'
 	$source_file = '\\\10.0.0.21\Instaladores\sqldeveloper-23.1.1.345.2114-x64.zip'
 	$destination_file = 'C:\\tmp\\sqldeveloper-23.1.1.345.2114-x64.zip'
 	$powershell_path = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
@@ -15,7 +15,7 @@ class instalacionapps::sqldeveloper {
 
 #script que lleva a cabo la copia y ejecución de la instalación del paquete
 	$contenidoScript = "
-		if (!(Test-Path -Path \"${directory_path}\SQLDeveloper")) {
+		if (!(Test-Path -Path \"${directory_path}\")) {
 			Copy-Item -Path \"${source_file}\" -Destination \"${destination_file}\"
 			Expand-Archive \"$destination_file\" -DestinationPath \"${directory_path}\"
 			Remove-Item -Path \"${destination_file}\"

9beea39915a33c19b6e0779c891c1f58f3802124
Javi Rufo <javirufo@iesvjp.es>
2025-09-23 18:35:01 +0200

    Backup automático: 2025-09-23 18:35:01

--- modules/dominio/manifests/fondo_linux.pp
+++ modules/dominio/manifests/fondo_linux.pp
@@ -6,8 +6,31 @@
 #   include dominio::fondo_linux
 class dominio::fondo_linux {
 
-  # Ruta destino del fondo en el sistema
   $fondo_destino = '/usr/share/backgrounds/fondo.jpg'
+
+  # Contenido de dconf para el fondo de escritorio
+  $dconf_content = "
+[org/gnome/desktop/background]
+picture-uri='file://${fondo_destino}'
+picture-uri-dark='file://${fondo_destino}'
+  "
+
+  $dconf_locks = "
+    /org/gnome/desktop/background/picture-uri
+    /org/gnome/desktop/background/picture-uri-dark
+"
+
+  # Contenido de lightdm.conf para forzar greeter
+  $lightdm_seat_conf = "
+    [Seat:*]
+    greeter-session=lightdm-gtk-greeter
+  "
+
+  # Contenido del greeter.conf para el fondo
+  $lightdm_greeter_conf = "
+    [greeter]
+    background=${fondo_destino}
+   "
 
   ###############################
   # Copiar el fondo desde Puppet
@@ -21,9 +44,8 @@ class dominio::fondo_linux {
   }
 
   ###############################
-  # Fondo de escritorio (usuarios)
+  # Fondo de escritorio GNOME
   ###############################
-
   file { '/etc/dconf/db/local.d':
     ensure => directory,
     owner  => 'root',
@@ -31,17 +53,12 @@ class dominio::fondo_linux {
     mode   => '0755',
   }
 
-  $contenidoback = "
-[org/gnome/desktop/background]
-picture-uri='file://${fondo_destino}'
-picture-uri-dark='file://${fondo_destino}'
-  "
   file { '/etc/dconf/db/local.d/00-background':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => $contenidoback,
+    content => $dconf_content,
   }
 
   file { '/etc/dconf/db/local.d/locks':
@@ -51,19 +68,12 @@ picture-uri-dark='file://${fondo_destino}'
     mode   => '0755',
   }
 
-
-
-	$contenidoback2 = "
-/org/gnome/desktop/background/picture-uri
-/org/gnome/desktop/background/picture-uri-dark
-	"
-
   file { '/etc/dconf/db/local.d/locks/background':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => $contenidoback2,
+    content => $dconf_locks,
   }
 
   exec { 'dconf_update_users':
@@ -77,21 +87,29 @@ picture-uri-dark='file://${fondo_destino}'
   }
 
   ########################################
-  # Fondo de LightDM (pantalla de login)
+  # Fondo LightDM (pantalla de login)
   ########################################
+  file { '/etc/lightdm/lightdm.conf.d':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 
-
- $contenidogreeter = "
-[greeter]
-background=${fondo_destino}
-"
+  file { '/etc/lightdm/lightdm.conf.d/99-mybackground.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => $lightdm_seat_conf,
+  }
 
   file { '/etc/lightdm/lightdm-gtk-greeter.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => $contenidogreeter,
+    content => $lightdm_greeter_conf,
     require => File[$fondo_destino],
   }
 

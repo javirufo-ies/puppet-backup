7f3e7b79fc92152265b4b33ea4422f209561cb17
Javi Rufo <javirufo@iesvjp.es>
2025-09-26 11:30:01 +0200

    Backup automático: 2025-09-26 11:30:01

--- modules/dominio/manifests/unirdominio.pp
+++ modules/dominio/manifests/unirdominio.pp
@@ -126,9 +126,6 @@ content => @("SCRIPT"/L)
 #!/bin/bash
 # Script PAM para añadir el usuario al grupo vboxusers
 GROUP="vboxusers"
-#USER="${PAM_USER:-$1}"
-#[ -z "$USER" ] && USER="$PAM_RUSER"
-
 # Añadir solo si no pertenece ya al grupo
 if ! id -nG "$USER" | grep -qw "$GROUP"; then
     usermod -aG "$GROUP" "$USER"

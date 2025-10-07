af1ae1416f74837a7209a0457f8a86299b297050
Javi Rufo <javirufo@iesvjp.es>
2025-10-07 12:05:01 +0200

    Backup automático: 2025-10-07 12:05:01

--- modules/instalacionapps/manifests/veyonmaster.pp
+++ modules/instalacionapps/manifests/veyonmaster.pp
@@ -39,25 +39,6 @@ else {
         ensure => installed,
       }
 
-      # Añadimos repositorio oficial de Veyon
-#      exec { 'add_veyon_repo':
-#        command => '/usr/bin/wget -O - https://veyon.io/key/veyon.gpg | apt-key add - && echo "deb http://ppa.launchpad.net/veyon/stable/ubuntu focal main" > /etc/apt/sources.list.d/veyon.list && apt-get update',
-#        creates => '/etc/apt/sources.list.d/veyon.list',
-#      }
-
-      # Instalamos el paquete Veyon Master
-#      package { 'veyon':
-#        ensure  => installed,
-#        require => Exec['add_veyon_repo'],
-#      }
-    
-
- # Servicio para que el equipo sea accesible desde otro
-#  service { 'veyon':
-#    ensure => running,
-#    enable => true,
-#    require => Package['veyon-service'],
-#  }
 
 # Copiar la configuración (JSON)
 	file { '/etc/veyon/veyon.json':
@@ -68,7 +49,56 @@ else {
 		source  => 'puppet:///modules/instalacionapps/veyon.json',
 #		require => Package['veyon'],
 		notify => Service['veyon'],
-  }
+	}
+# Manifiesto Puppet para desplegar acceso directo global de Veyon Master (Ubuntu + Cinnamon)
+file { '/usr/share/applications/veyon-master.desktop':
+  ensure  => file,
+  mode    => '0755',
+  content => "[Desktop Entry]
+Type=Application
+Name=Veyon Master
+Exec=/usr/bin/veyon-master
+Icon=veyon
+Terminal=false
+Categories=Education;Utility;
+Comment=Lanzar Veyon Master
+",
+}
+
+# Copiar el icono a /etc/skel (para nuevos usuarios locales o AD cacheados)
+file { '/etc/skel/Escritorio/veyon-master.desktop':
+  ensure => link,
+  target => '/usr/share/applications/veyon-master.desktop',
+}
+
+# Crear enlace para todos los usuarios existentes locales
+exec { 'deploy_veyon_icon_existing_users':
+  command => '/bin/bash -c "for d in /home/*/Escritorio; do ln -sf /usr/share/applications/veyon-master.desktop $d/; chmod +x $d/veyon-master.desktop; done"',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => '/usr/bin/test -d /home',
+}
+
+# Crear script que añade el icono cuando un usuario de AD inicia sesión (si su carpeta home aún no existía)
+file { '/etc/profile.d/veyon_icon.sh':
+  ensure  => file,
+  mode    => '0755',
+  content => '#!/bin/bash
+DESKTOP_DIR="$HOME/Escritorio"
+ICON="/usr/share/applications/veyon-master.desktop"
+if [ -d "$DESKTOP_DIR" ] && [ ! -e "$DESKTOP_DIR/veyon-master.desktop" ]; then
+  ln -sf "$ICON" "$DESKTOP_DIR/"
+  chmod +x "$DESKTOP_DIR/veyon-master.desktop"
+fi
+',
+}
+
+
+
+
+
+
+
+
 }
 
 

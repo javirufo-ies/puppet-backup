015de480740b6a30822078b434e8bfcaa1a7637d
Javi Rufo <javirufo@iesvjp.es>
2025-10-23 16:55:01 +0200

    Backup automático: 2025-10-23 16:55:01

--- modules/equipos/manifests/smr2d.pp
+++ modules/equipos/manifests/smr2d.pp
@@ -16,5 +16,25 @@ class equipos::smr2d {
 		package {'xampp-81':
 			ensure => present,
 		}
+
+
+ # --- Instalar Jupyter e ipykernel mediante pip ---
+  exec { 'install jupyter':
+    command => 'powershell -NoProfile -ExecutionPolicy Bypass -Command "python -m pip install --upgrade pip jupyter ipykernel"',
+    unless  => 'powershell -NoProfile -Command "(pip show jupyter) -ne $null"',
+    path    => ['C:\Python311', 'C:\Python311\Scripts', 'C:\Windows\System32'],
+    require => Package['python'],
+  }
+
+  # --- Registrar el kernel (para VSCode o JupyterLab) ---
+  exec { 'register ipykernel':
+    command => 'powershell -NoProfile -ExecutionPolicy Bypass -Command "python -m ipykernel install --user"',
+    unless  => 'powershell -NoProfile -Command "Test-Path $env:USERPROFILE\.local\share\jupyter\kernels\python3"',
+    path    => ['C:\Python311', 'C:\Python311\Scripts', 'C:\Windows\System32'],
+    require => Exec['install jupyter'],
+  }
+
+
+
          }
 }

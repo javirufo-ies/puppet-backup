13e9a07e23ddc0af6df30f34ad2d352bcee740c6
Javi Rufo <javirufo@iesvjp.es>
2025-10-24 11:45:01 +0200

    Backup automático: 2025-10-24 11:45:01

--- modules/equipos/manifests/smr2d.pp
+++ modules/equipos/manifests/smr2d.pp
@@ -18,14 +18,14 @@ class equipos::smr2d {
 		}
 
 	exec { 'install jupyter':
-		command => 'python -m pip install --upgrade jupyter ipykernel',
+		command => 'c:\\Python313\\python.exe -m pip install --upgrade jupyter ipykernel',
 		path    => ['C:\\Windows\\System32','C:\\Windows\\System32\\WindowsPowerShell\\v1.0','C:\\Python\\313','C:\\Python313\\Scripts'],
-		unless  => 'python -m pip show jupyter',
+		unless  => 'c:\\Python313\\python.exe -m pip show jupyter',
 	}
 
   # egistrar kernel de Jupyter para VS Code
 	exec { 'register jupyter kernel':
-		command => 'python -m ipykernel install --user --name=python --display-name "Python (Jupyter)"',
+		command => 'c:\\Python313\\python.exe -m ipykernel install --user --name=python --display-name "Python (Jupyter)"',
 		path    => ['C:\\Windows\\System32','C:\\Windows\\System32\\WindowsPowerShell\\v1.0','C:\\Python\\313','C:\\Python313\\Scripts'],
 		unless  => 'if exist "%USERPROFILE%\\.local\\share\\jupyter\\kernels\\python" (exit 0) else (exit 1)',
 	}

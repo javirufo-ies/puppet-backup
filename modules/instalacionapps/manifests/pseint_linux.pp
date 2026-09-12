9d24e37ceeaa46fba0e33d7b6830e409b6797ccf
Javi Rufo <javirufo@educarex.es>
2026-09-12 17:10:02 +0000

    Auto commit diario

--- modules/instalacionapps/manifests/pseint_linux.pp
+++ modules/instalacionapps/manifests/pseint_linux.pp
@@ -9,22 +9,25 @@ class instalacionapps::pseint_linux {
   $version = '20250314'
 
   package { [
-    'wget',
     'tar',
   ]:
     ensure => installed,
   }
 
   exec { 'descargar_pseint':
-    command => "wget -O /tmp/pseint.tgz https://downloads.sourceforge.net/project/pseint/PSeInt/${version}/pseint-l64-${version}.tgz",
+    command => "/usr/bin/wget -O /tmp/pseint.tgz https://downloads.sourceforge.net/project/pseint/PSeInt/${version}/pseint-l64-${version}.tgz",
     creates => '/tmp/pseint.tgz',
     require => Package['wget'],
+	path    => ['/usr/bin', '/bin'],
+
   }
 
   exec { 'instalar_pseint':
     command => 'rm -rf /opt/pseint && mkdir -p /opt && tar -xzf /tmp/pseint.tgz -C /opt && mv /opt/pseint* /opt/pseint',
     creates => '/opt/pseint/pseint',
     require => Exec['descargar_pseint'],
+	  path    => ['/usr/bin', '/bin'],
+
   }
 
   file { '/usr/local/bin/pseint':
@@ -46,6 +49,7 @@ Type=Application
 Categories=Education;Development;
 Icon=/opt/pseint/imgs/pseint.png
 DESKTOP
+,
     require => Exec['instalar_pseint'],
   }
 

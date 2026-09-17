49b37384e329396f7ed7ab203960295e3f0cc242
Javi Rufo <javirufo@educarex.es>
2026-09-17 13:10:01 +0000

    Auto commit diario

--- modules/instalacionapps/manifests/mysqlworkbench_linux.pp
+++ modules/instalacionapps/manifests/mysqlworkbench_linux.pp
@@ -1,16 +1,35 @@
 class instalacionapps::mysqlworkbench_linux {
 
 
-mysql_user { 'alumno@localhost':
-  ensure        => present,
-  password_hash => mysql::password('12345678'),
+  exec { 'crear_usuario_alumno_mysql':
+    command => "/usr/bin/mysql -e \"CREATE USER IF NOT EXISTS 'alumno'@'localhost' IDENTIFIED BY '12345678';\"",
+    unless  => "/usr/bin/mysql -NBe \"SELECT User FROM mysql.user WHERE User='alumno' AND Host='localhost'\" | /bin/grep -q '^alumno\$'",
+    path    => ['/usr/bin', '/bin'],
+  }
+
+  exec { 'permisos_usuario_alumno_mysql':
+    command => "/usr/bin/mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'alumno'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;\"",
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['crear_usuario_alumno_mysql'],
+  }
+
+
+package { 'snapd':
+  ensure => installed,
 }
 
-mysql_grant { 'alumno@localhost/*.*':
-  ensure     => present,
-  privileges => ['ALL'],
-  table      => '*.*',
-  user       => 'alumno@localhost',
+exec { 'instalar_mysql_workbench':
+  command => '/usr/bin/snap install mysql-workbench-community',
+  unless  => '/usr/bin/snap list mysql-workbench-community',
+  require => Package['snapd'],
+  path    => ['/usr/bin','/bin'],
+}
+
+exec { 'mysql_workbench_password_manager':
+  command => '/usr/bin/snap connect mysql-workbench-community:password-manager-service',
+  unless  => '/usr/bin/snap connections mysql-workbench-community | grep password-manager-service | grep -q :password-manager-service',
+  require => Exec['instalar_mysql_workbench'],
+  path    => ['/usr/bin','/bin'],
 }
 
 }
